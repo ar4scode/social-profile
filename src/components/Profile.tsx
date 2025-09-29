d978b95f659a8164ca8ed5eb591ea12f7c5e67fd
Ar4sCode <ar4sazizi@gmail.com>
2025-09-29 23:57:53 +0330

    create declaration file to declare the type of the image

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,10 @@
-
+import profileImage from "../assets/IMG_8607.JPG"
 
 const Profile = () => {
   return (
     <div className=" p-4 flex justify-center items-center gap-6">
       {/* we don't need to import image if image is inside the public folder */}
-      <img className="w-[120px] h-[120px] rounded-full object-cover border-[#CFCFEA] border-4" src="/IMG_8607.JPG" alt="Aras profile image" />
+      <img className="w-[120px] h-[120px] rounded-full object-cover border-[#CFCFEA] border-4" src={profileImage} alt="Aras profile image" />
       <div>
         <p className="text-2xl font-semibold">Aras Azizi</p>
         <p className="text- mt-2 pl-2">Frontend Developer</p>
