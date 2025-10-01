b4ed6fb7edb8cd682530c53b0300d0354ca1eb7c
Ar4sCode <ar4sazizi@gmail.com>
2025-10-01 13:44:48 +0330

    Change the background of page and make components background blur

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,15 @@ import Projects from './components/Projects';
 
 function App() {
   
-
   return (
-    <div className='m-4 rounded-2xl shadow-md shadow-[#CFCFEA]'>
+    <div className="md:mx-auto m-4 md:max-w-2/4 rounded-2xl shadow-md bg-[#26282c]/60 backdrop-blur-md">
       <Profile />
       <About />
       <Socials />
       <Skills />
       <Projects />
     </div>
+
   )
 }
 
