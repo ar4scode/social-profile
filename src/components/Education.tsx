d4af8c79e52002c6f2ff0cd1edaa36177d0ed8a4
ar4scode <ar4sazizi@gmail.com>
2025-10-08 00:19:19 +0330

    Add university logo to education section

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,10 +1,15 @@
+import universityPic from "../assets/uni.png"
+
 function Education() {
     return (
         <div className="p-6">
-            <p className="text-sm mx-2 text-gray-400">/Education</p>
-            <div className="mx-2 mt-2 text-sm">
-                <h4>Bachelor’s in Computer Networks Engineering</h4>
-                <p className="text-xs text-gray-400 italic pt-1">Kermanshah National Skills University — 2025 – Present</p>
+            <p className="text-sm mx-4 text-gray-400">/Education</p>
+            <div className="mx-8 mt-2 text-sm flex items-center gap-4 ">
+                <img className="w-8 h-8 md:w-12 md:h-12" src={universityPic} alt="" />
+                <div>
+                    <h4>Bachelor’s in Computer Networks Engineering</h4>
+                    <p className="text-xs text-gray-400 italic pt-1">Kermanshah National Skills University — 2025 – Present</p>
+                </div>
             </div>
         </div>
     )
